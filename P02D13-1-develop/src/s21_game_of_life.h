d7d0370b47b83759eab9b9a5293a28fb36e6f03d
Maksim Nikolin <maksim1nikolin@gmail.com>
2024-04-05 09:34:39 +0500

    Add files

---
+++ P02D13-1-develop/src/s21_game_of_life.h
@@ -0,0 +1,21 @@
+#ifndef _SRC_S21_GAME_OF_LIFE_H_
+#define _SRC_S21_GAME_OF_LIFE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>     // for strncmp
+#include <sys/epoll.h>  // for epoll_create1(), epoll_ctl(), struct epoll_event
+#include <time.h>
+#include <unistd.h>
+
+#define MAX_EVENTS 5
+#define READ_SIZE 10
+
+#define SIZE_X 80
+#define SIZE_Y 25
+
+#define CHAR_ALIVE '#'
+#define CHAR_DEAD '.'
+
+#endif  // _SRC_S21_GAME_OF_LIFE_H_
+
