a82f45744d4c09e2b4cc358008572491e423163f
Maksim Nikolin <maksim1nikolin@gmail.com>
2024-04-11 04:53:39 +0500

    Add files via upload

---
+++ C3_SimpleBashUtils-1/src/cat/s21_cat.h
@@ -0,0 +1,17 @@
+#ifndef SRC_CAT_S21_CAT_H_
+#define SRC_CAT_S21_CAT_H_
+
+#include <getopt.h>
+#include <stdio.h>
+#include <unistd.h>
+
+struct flags {
+  int b, v, e, n, s, t;
+};
+
+void print_logo();
+void cat_flags(int argc, char* argv[], struct flags* flag);
+void open_file(int argc, char* argv[], struct flags flag);
+void use_flags(FILE* file, struct flags flag, int* lines_count, int* prev_ch);
+
+#endif
