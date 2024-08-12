f74f3a70ae599db6cd9ebc71210c890c9903c847
Maksim Nikolin <maksim1nikolin@gmail.com>
2024-08-12 18:48:50 +0500

    Delete DO6_CICD-1-develop/notify.sh

--- DO6_CICD-1-develop/notify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TELEGRAM_BOT_TOKEN="6878559767:AAHLqLeGlQ9YAtfZM3REJhehMPbl_HOLByY"
-TELEGRAM_USER_ID="153656092"
-TIME="10"
-URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-TEXT="Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
-
-curl -s --max-time $TIME -d  "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
