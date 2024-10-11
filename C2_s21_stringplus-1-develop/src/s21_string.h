b9f8ce60db33ce95af802e0e2184abe72e139b19
Maksim Nikolin <maksim1nikolin@gmail.com>
2024-10-12 01:28:41 +0500

    Update s21_string.h

--- C2_s21_stringplus-1-develop/src/s21_string.h
+++ C2_s21_stringplus-1-develop/src/s21_string.h
@@ -8,7 +8,7 @@
 #define s21_NULL (void *)0
 #define s21_bool int
 
-void *s21_memchr(const void *str, int c, s21_size_t n);  // yonnrose
+void *s21_memchr(const void *str, int c, s21_size_t n);                       // yonnrose
 int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
 void *s21_memcpy(void *dest, const void *src, s21_size_t n);
 void *s21_memset(void *str, int c, s21_size_t n);
@@ -21,19 +21,18 @@ void *s21_trim(const char *src, const char *trim_chars);
 int firstindex(const char *src, const char *totrim, int start);
 int lastindex(const char *src, const char *totrim, int end);
 
-s21_size_t s21_strlen(const char *str);  // daisymal
+s21_size_t s21_strlen(const char *str);                                       // daisymal
 char *s21_strncat(char *str1, const char *str2, s21_size_t n);
 char *s21_strchr(const char *str, int c);
 int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
-void print_logo();
 
-void *s21_to_upper(const char *str);  // kokoschl
+void *s21_to_upper(const char *str);                                          // kokoschl
 void *s21_to_lower(const char *str);
 char *s21_strtok(char *str, const char *delim);
 char *s21_strstr(const char *haystack, const char *needle);
 char *s21_strrchr(const char *str, int c);
 
-#include "s21_sprintf.h"  // ilynpois
+#include "s21_sprintf.h"                                                      // ilynpois
 #include "s21_sscanf.h"
 
 #endif  // SRC_S21_STRING_H_
