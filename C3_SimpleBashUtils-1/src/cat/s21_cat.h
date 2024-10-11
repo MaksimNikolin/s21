21229e69b97179c83d83de6e0abb737252016823
Maksim Nikolin <maksim1nikolin@gmail.com>
2024-10-11 16:28:04 +0500

    Update s21_cat.h

--- C3_SimpleBashUtils-1/src/cat/s21_cat.h
+++ C3_SimpleBashUtils-1/src/cat/s21_cat.h
@@ -9,9 +9,8 @@ struct flags {
   int b, v, e, n, s, t;
 };
 
-void print_logo();
 void cat_flags(int argc, char* argv[], struct flags* flag);
 void open_file(int argc, char* argv[], struct flags flag);
 void use_flags(FILE* file, struct flags flag, int* lines_count, int* prev_ch);
 
-#endif
+#endif // SRC_CAT_S21_CAT_H_
